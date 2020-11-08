9a50661f0317482ecc991f2213d66597d43b74c4
Cameron Moore <cam@cameronlpmoore.com>
2020-11-07 16:37:54 -0800

    timer-app: adds Timer class implementation

---
+++ timer-app/Timer.js
@@ -0,0 +1,68 @@
+/*
+The timer-app will implement the following:
+1. Displays a timer
+2. Shows an animated border around the timer
+
+-> Event-based architecture
+-> Class-based implementation
+
+class Timer
+{
+    // Constructor
+    constructor(durationInput, startButton, pauseButton)
+    // Methods
+    start()
+    pause()
+    onDurationChange()
+    tick()
+}
+*/
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+    
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        if (this.onStart) {
+            this.onStart();
+        }
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    pause = () => {
+        clearInterval(this.interval);
+    }
+
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
+        } else {
+            this.timeRemaining = this.timeRemaining - 1;
+            if (this.onTick) {
+                this.onTick();
+            }
+        }
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    }
+}
--- timer-app/index.js
+++ timer-app/index.js
@@ -1,40 +1,15 @@
-/*
-Things our program does:
-1. Displays a timer
-2. Shows an animated border around the timer
-
--> Event-based architecture
--> Class-based implementation
-
-class Timer
-{
-    // Constructor
-    constructor(durationInput, startButton, pauseButton)
-    // Methods
-    start()
-    pause()
-    onDurationChange()
-    tick()
-}
-*/
-class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-    
-        this.startButton.addEventListener('click', this.start);
-    }
-
-    start = () => {
-        console.log(this);
-    }
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
-timer.start();
-
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart() {
+        console.log('timer started...');
+    },
+    onTick() {
+        console.log('timer just ticked down')
+    },
+    onComplete() {
+        console.log('completed...')
+    }
+});
